1026683a43e7b2076510f94b20819e40015edcc0
serufim <serufim97@yandex.ru>
2019-08-16 18:05:54 +0800

    Добавил первый роут на вставку данных в таблицу
    Выглядит красиво

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     MetaData, Table, Column, ForeignKey,
     Integer, String, Date
 )
+from sqlalchemy.dialects.postgresql import insert
 
 metadata = MetaData()
 
@@ -47,3 +48,32 @@ async def init_pg(app):
 async def close_pg(app):
     app['db'].close()
     await app['db'].wait_closed()
+
+
+async def insert_import(conn, new_citizens, new_relatives):
+    """Вставка импорта"""
+    import_id = None
+    async with conn.begin():
+        # Cначала вставляем запись в таблицу imports и получаем id
+        result = await conn.execute(imports.insert().values().returning(imports.c.id))
+        import_id = await result.scalar()
+
+        for new_citizen in new_citizens:
+            new_citizen['import_id'] = import_id
+
+        # Всталяем всех горожан за один множественный инсерт
+        result = await conn.execute(
+            insert(citizens).returning(citizens.c.id, citizens.c.citizen_id).values(
+                new_citizens).on_conflict_do_nothing())  # Фишка постгреса returning в insert
+
+        inserted_citizens = await result.fetchall()
+        citizen_mapper = {item[1]: item[0] for item in inserted_citizens}
+        values = []
+        for new_citizen in new_citizens:
+            for relative in new_relatives[new_citizen['citizen_id']]:
+                values.append({
+                    'citizen_id': citizen_mapper[new_citizen['citizen_id']],
+                    'relative_id': citizen_mapper[relative]})
+
+        await conn.execute(insert(relatives).returning(relatives.c.id).values(values).on_conflict_do_nothing())
+    return import_id
--- api/views.py
+++ api/views.py
@@ -1,9 +1,24 @@
 from aiohttp import web
 from aiohttp_apispec import request_schema
 
+from .models import insert_import
 from .schemas import ImportsSchema
 
 
 @request_schema(ImportsSchema())
-def add_import(request):
-    return web.Response(text="Привет мир")
+async def add_import(request):
+    relatives = {}
+    citizens = request['data']['citizens']
+
+    for citizen in citizens:
+        relatives[citizen['citizen_id']] = citizen['relatives']
+        del citizen['relatives']
+
+    async with request.app['db'].acquire() as conn:
+        import_id = await insert_import(conn, citizens, relatives)
+
+        if import_id is None:
+            return web.json_response("an errow was acquired", status=400)
+
+        output_data = {"data": {"import_id": import_id}}
+        return web.json_response(output_data, status=201)
