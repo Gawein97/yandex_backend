94358622fa03200a01dc5d02b665d71dfe677655
serufim <serufim97@yandex.ru>
2019-08-19 03:41:02 +0800

    Обновил метод для запсука

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import argparse
+
 from aiohttp import web
 from aiohttp_apispec import validation_middleware, setup_aiohttp_apispec
 from aiohttp_route_middleware import UrlDispatcherEx
@@ -8,7 +10,7 @@ from api.schemas import my_error_handler
 from api.settings import get_config
 
 
-async def init_app(argv=None):
+async def init_gunicorn_app(argv=None):
     app = web.Application(router=UrlDispatcherEx())
     app['config'] = get_config(argv)
     setup_aiohttp_apispec(app=app, error_callback=my_error_handler)
@@ -19,5 +21,21 @@ async def init_app(argv=None):
     return app
 
 
+async def init_deploy_app(argv=None):
+    args = parser.parse_args()
+    app = web.Application(router=UrlDispatcherEx())
+    app['config'] = get_config(argv)
+    setup_aiohttp_apispec(app=app, error_callback=my_error_handler)
+    app.middlewares.append(validation_middleware)
+    app.on_startup.append(init_pg)
+    app.on_cleanup.append(close_pg)
+    setup_routes(app)
+    web.run_app(app, path=args.path, port=args.port)
+
+
 if __name__ == '__main__':
-    init_app()
+    parser = argparse.ArgumentParser(description="aiohttp server example")
+    parser.add_argument('--path')
+    parser.add_argument('--port')
+
+    init_deploy_app()
